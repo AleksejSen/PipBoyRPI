73473a21400228d0600a0a0939e067495dd00d1d
Alex <sinbar82@icloud.com>
2024-05-09 13:36:32 +0200

    add: buttons

--- pip.py
+++ pip.py
@@ -12,6 +12,7 @@ from PyQt5.QtWidgets import (
    QLabel,
    QVBoxLayout, 
    QHBoxLayout,
+   QButtonGroup
 ) 
 from PyQt5.QtGui import QMovie, QPalette, QColor
 
@@ -38,12 +39,17 @@ class MainWindow(QMainWindow):
         central_widget.setLayout(self.main_layout)
 
         #gif animation
+        mainWindowStyles = """
+            border: none;
+            border-top : 3px solid green;
+        """
+        
         self.movie = QMovie("images/vaultboy.gif")
         valutBoyLable = QLabel(self)
         valutBoyLable.setMovie(self.movie)
         self.movie.start()
         valutBoyLable.setAlignment(Qt.AlignCenter)
-
+        valutBoyLable.setStyleSheet(mainWindowStyles)
         self.main_layout.addWidget(valutBoyLable)
 
         #Buttons
@@ -51,30 +57,64 @@ class MainWindow(QMainWindow):
         btnHsize = 40
         btnWsize = 80
 
+        buttonStyleStr = """
+        QPushButton
+        {
+            color : green; 
+            border: none; 
+        }
+        QPushButton::checked
+        {
+            color : green; 
+            border: none;
+            border-top : 3px solid green;
+            border-left : 3px solid green;
+            border-right : 3px solid green;
+            border-top-left-radius: 7px;
+            border-top-right-radius: 7px;
+            font-weight: bold
+        }
+        """
+            
+
         statBtn = QPushButton(text="STAT", parent=self)
         statBtn.setFixedSize(btnWsize, btnHsize)        
-        statBtn.setStyleSheet("color : green; border: none; font-weight: bold") 
+        statBtn.setStyleSheet(buttonStyleStr) 
         statBtn.setFont(QFont('Monofonto', btnFontSize))
+        statBtn.setCheckable(True)
+        statBtn.setChecked(True)
 
         invBtn = QPushButton(text="INV", parent=self)
         invBtn.setFixedSize(btnWsize, btnHsize)        
-        invBtn.setStyleSheet("color : green; border: none; font-weight: bold") 
+        invBtn.setStyleSheet(buttonStyleStr) 
         invBtn.setFont(QFont('Monofonto', btnFontSize))
+        invBtn.setCheckable(True)
         
         dataBtn = QPushButton(text="DATA", parent=self)
         dataBtn.setFixedSize(btnWsize, btnHsize)        
-        dataBtn.setStyleSheet("color : green; border: none; font-weight: bold") 
+        dataBtn.setStyleSheet(buttonStyleStr) 
         dataBtn.setFont(QFont('Monofonto', btnFontSize))
+        dataBtn.setCheckable(True)
 
         mapBtn = QPushButton(text="MAP", parent=self)
         mapBtn.setFixedSize(btnWsize, btnHsize)        
-        mapBtn.setStyleSheet("color : green; border: none; font-weight: bold") 
+        mapBtn.setStyleSheet(buttonStyleStr) 
         mapBtn.setFont(QFont('Monofonto', btnFontSize))
+        mapBtn.setCheckable(True)
         
         radioBtn = QPushButton(text="RADIO", parent=self)
         radioBtn.setFixedSize(btnWsize, btnHsize)        
-        radioBtn.setStyleSheet("color : green; border: none; font-weight: bold") 
+        radioBtn.setStyleSheet(buttonStyleStr) 
         radioBtn.setFont(QFont('Monofonto', btnFontSize))
+        radioBtn.setCheckable(True)
+
+        #button group to chec/unchek buttons
+        btn_group = QButtonGroup(self)
+        btn_group.addButton(statBtn)
+        btn_group.addButton(invBtn)
+        btn_group.addButton(dataBtn)
+        btn_group.addButton(mapBtn)
+        btn_group.addButton(radioBtn)
 
         self.top_bar.addWidget(statBtn)
         self.top_bar.addWidget(invBtn)
