8375c47e617b77c20bbcfdd7c842e939443ddb21
Alex <sinbar82@icloud.com>
2024-05-09 20:30:15 +0200

    add tab layout

--- pip.py
+++ pip.py
@@ -1,19 +1,19 @@
-#images from: https://github.com/ultrinnan/PipBoy?tab=readme-ov-file
+# images from: https://github.com/ultrinnan/PipBoy?tab=readme-ov-file
 import sys
 import os
 
 from PyQt5.QtCore import QSize, Qt
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import (
-   QApplication, 
-   QMainWindow, 
-   QWidget, 
-   QPushButton, 
-   QLabel,
-   QVBoxLayout, 
-   QHBoxLayout,
-   QButtonGroup
-) 
+    QApplication,
+    QMainWindow,
+    QWidget,
+    QPushButton,
+    QLabel,
+    QVBoxLayout,
+    QHBoxLayout,
+    QButtonGroup,
+)
 from PyQt5.QtGui import QMovie, QPalette, QColor
 
 
@@ -25,34 +25,41 @@ class MainWindow(QMainWindow):
         # os.system('export DISPLAY=:0')
 
         self.setWindowTitle("PipBoy")
-        self.setStyleSheet("background-color: black;") 
+        self.setStyleSheet("background-color: black;")
         self.setWindowFlags(Qt.FramelessWindowHint)
 
         self.setFixedSize(QSize(480, 320))
         self.top_bar = QHBoxLayout()
         self.main_layout = QVBoxLayout()
         self.main_layout.addLayout(self.top_bar)
+        self.main_layout.setSpacing(0)
 
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
         # Set the layout on the central widget
         central_widget.setLayout(self.main_layout)
 
-        #gif animation
+        # STAT Menue
         mainWindowStyles = """
             border: none;
             border-top : 3px solid green;
         """
-        
         self.movie = QMovie("images/vaultboy.gif")
         valutBoyLable = QLabel(self)
         valutBoyLable.setMovie(self.movie)
         self.movie.start()
         valutBoyLable.setAlignment(Qt.AlignCenter)
         valutBoyLable.setStyleSheet(mainWindowStyles)
+        # self.main_layout.insertWidget(0, valutBoyLable)
         self.main_layout.addWidget(valutBoyLable)
 
-        #Buttons
+        # INV Menue
+        # self.anotherLabel = QLabel("Another Label", self)
+        # self.anotherLabel.setAlignment(Qt.AlignCenter)
+        # self.anotherLabel.setStyleSheet(mainWindowStyles)
+        # self.anotherLabel.hide()  # Hide it initially
+        # self.main_layout.addWidget(self.anotherLabel)
+
         btnFontSize = 15
         btnHsize = 40
         btnWsize = 80
@@ -60,55 +67,57 @@ class MainWindow(QMainWindow):
         buttonStyleStr = """
         QPushButton
         {
-            color : green; 
-            border: none; 
+            font-family: Monofonto, serif;
+            font-size: 15px;
+            color : green;
+            border: none;
+            border-bottom : 3px solid green;
+            margin-bottom: -10px
         }
         QPushButton::checked
         {
-            color : green; 
+            color : green;
             border: none;
             border-top : 3px solid green;
             border-left : 3px solid green;
             border-right : 3px solid green;
             border-top-left-radius: 7px;
             border-top-right-radius: 7px;
-            font-weight: bold
+            border-bottom : 3px solid green;
+            font-weight: bold;
+            font-size: 16px;
+            margin-bottom: -10px;
+            padding-bottom: 5px;
         }
         """
-            
 
         statBtn = QPushButton(text="STAT", parent=self)
-        statBtn.setFixedSize(btnWsize, btnHsize)        
-        statBtn.setStyleSheet(buttonStyleStr) 
-        statBtn.setFont(QFont('Monofonto', btnFontSize))
+        statBtn.setFixedSize(btnWsize, btnHsize)
+        statBtn.setStyleSheet(buttonStyleStr)
         statBtn.setCheckable(True)
         statBtn.setChecked(True)
 
         invBtn = QPushButton(text="INV", parent=self)
-        invBtn.setFixedSize(btnWsize, btnHsize)        
-        invBtn.setStyleSheet(buttonStyleStr) 
-        invBtn.setFont(QFont('Monofonto', btnFontSize))
+        invBtn.setFixedSize(btnWsize, btnHsize)
+        invBtn.setStyleSheet(buttonStyleStr)
         invBtn.setCheckable(True)
-        
+
         dataBtn = QPushButton(text="DATA", parent=self)
-        dataBtn.setFixedSize(btnWsize, btnHsize)        
-        dataBtn.setStyleSheet(buttonStyleStr) 
-        dataBtn.setFont(QFont('Monofonto', btnFontSize))
+        dataBtn.setFixedSize(btnWsize, btnHsize)
+        dataBtn.setStyleSheet(buttonStyleStr)
         dataBtn.setCheckable(True)
 
         mapBtn = QPushButton(text="MAP", parent=self)
-        mapBtn.setFixedSize(btnWsize, btnHsize)        
-        mapBtn.setStyleSheet(buttonStyleStr) 
-        mapBtn.setFont(QFont('Monofonto', btnFontSize))
+        mapBtn.setFixedSize(btnWsize, btnHsize)
+        mapBtn.setStyleSheet(buttonStyleStr)
         mapBtn.setCheckable(True)
-        
+
         radioBtn = QPushButton(text="RADIO", parent=self)
-        radioBtn.setFixedSize(btnWsize, btnHsize)        
-        radioBtn.setStyleSheet(buttonStyleStr) 
-        radioBtn.setFont(QFont('Monofonto', btnFontSize))
+        radioBtn.setFixedSize(btnWsize, btnHsize)
+        radioBtn.setStyleSheet(buttonStyleStr)
         radioBtn.setCheckable(True)
 
-        #button group to chec/unchek buttons
+        # button group to chec/unchek buttons
         btn_group = QButtonGroup(self)
         btn_group.addButton(statBtn)
         btn_group.addButton(invBtn)
@@ -129,3 +138,8 @@ window = MainWindow()
 window.show()
 
 app.exec()
+
+            # border-top : 3px solid green;
+            # border-left : 3px solid green;
+            # border-right : 3px solid green;
+            # border-top-left-radius: 7px;
---
+++ tab_pipboy.py
@@ -0,0 +1,87 @@
+import sys
+import os
+
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtWidgets import (
+    QApplication,
+    QCheckBox,
+    QTabWidget,
+    QVBoxLayout,
+    QWidget,
+)
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("PiP Boy")
+        self.resize(480, 320)
+        self.setStyleSheet("background-color: black;")
+        self.setWindowFlags(Qt.FramelessWindowHint)
+
+        tabBarBarStyle = """
+        QTabWidget::pane{
+            border: 3px solid green;
+            border-radius: 7px;
+        }
+        QTabBar::tab {
+            width: 85px; 
+        }
+        QTabBar {
+            alignment: center; 
+            font-family: Monofonto, serif;
+            font-size: 15px;
+            color : green;
+        }
+        QTabBar::tab-bar {
+            border: none;
+        }
+        QTabBar::tab:selected {
+            color : green;
+            font-size: 16px; 
+            font-weight: bold;
+            font-size: 16px;
+            border: none;
+            border-top : 3px solid green;
+            border-bottom : 3px solid green;
+            border-radius: 7px;
+        }
+        """
+        # Create a top-level layout
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+        # Create the tab widget with two tabs
+        tabs = QTabWidget()
+        tabs.setStyleSheet(tabBarBarStyle)
+
+        tabs.addTab(self.generalTabUI(), "STAT")
+        tabs.addTab(self.networkTabUI(), "INV")
+        tabs.addTab(self.networkTabUI(), "DATA")
+        tabs.addTab(self.networkTabUI(), "MAP")
+        tabs.addTab(self.networkTabUI(), "RADIO")
+        layout.addWidget(tabs)
+
+
+    def generalTabUI(self):
+        """Create the General page UI."""
+        tab = QWidget()
+        layout = QVBoxLayout()
+        layout.addWidget(QCheckBox("General Option 1"))
+        layout.addWidget(QCheckBox("General Option 2"))
+        # tab.setStyleSheet(self.tabStyles)
+        tab.setLayout(layout)
+        return tab
+
+    def networkTabUI(self):
+        """Create the Network page UI."""
+        networkTab = QWidget()
+        layout = QVBoxLayout()
+        layout.addWidget(QCheckBox("Network Option 1"))
+        layout.addWidget(QCheckBox("Network Option 2"))
+        networkTab.setLayout(layout)
+        return networkTab
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
