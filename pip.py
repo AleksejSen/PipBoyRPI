e764d1f8173222d475e672b2c5cc1169308d1060
Alex <sinbar82@icloud.com>
2024-05-09 11:52:24 +0200

    add basic layouts

--- pip.py
+++ pip.py
@@ -3,8 +3,16 @@ import os
 
 from PyQt5.QtCore import QSize, Qt
 from PyQt5.QtGui import *
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
-from PyQt5.QtGui import QMovie
+from PyQt5.QtWidgets import (
+   QApplication, 
+   QMainWindow, 
+   QWidget, 
+   QPushButton, 
+   QLabel,
+   QVBoxLayout, 
+   QHBoxLayout,
+) 
+from PyQt5.QtGui import QMovie, QPalette, QColor
 
 
 # Subclass QMainWindow to customize your application's main window
@@ -18,40 +26,60 @@ class MainWindow(QMainWindow):
         self.setStyleSheet("background-color: black;") 
         self.setWindowFlags(Qt.FramelessWindowHint)
 
-        # button = QPushButton("Press Me!")
-
         self.setFixedSize(QSize(480, 320))
+        self.top_bar = QHBoxLayout()
+        self.main_layout = QVBoxLayout()
+        self.main_layout.addLayout(self.top_bar)
+
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+        # Set the layout on the central widget
+        central_widget.setLayout(self.main_layout)
 
         #gif animation
         self.movie = QMovie("images/vaultboy.gif")
-        label = QLabel(self)
-        label.setMovie(self.movie)
+        valutBoyLable = QLabel(self)
+        valutBoyLable.setMovie(self.movie)
         self.movie.start()
-        label.setAlignment(Qt.AlignCenter)
-        self.setCentralWidget(label)
+        valutBoyLable.setAlignment(Qt.AlignCenter)
+
+        self.main_layout.addWidget(valutBoyLable)
 
         #Buttons
+        btnFontSize = 15
+        btnHsize = 40
+        btnWsize = 80
+
         statBtn = QPushButton(text="STAT", parent=self)
-        statBtn.setFixedSize(100, 60)        
+        statBtn.setFixedSize(btnWsize, btnHsize)        
         statBtn.setStyleSheet("color : green; border: none; font-weight: bold") 
-        statBtn.setFont(QFont('Monofonto', 17))
+        statBtn.setFont(QFont('Monofonto', btnFontSize))
 
-        # invBtn = QPushButton(text="INV", parent=self)
-        # invBtn.setFixedSize(100, 60)        
-        # invBtn.setStyleSheet("color : green; border: none; font-weight: bold") 
-        # invBtn.setFont(QFont('Monofonto', 17))
-
-        #static image
-        # pixmap = QPixmap('images/vaultboy.gif')
-        # label = QLabel(self)
-        # label.setPixmap(pixmap)
-        # label.setAlignment(Qt.AlignCenter)
+        invBtn = QPushButton(text="INV", parent=self)
+        invBtn.setFixedSize(btnWsize, btnHsize)        
+        invBtn.setStyleSheet("color : green; border: none; font-weight: bold") 
+        invBtn.setFont(QFont('Monofonto', btnFontSize))
         
-        # self.setCentralWidget(label)
-        # self.resize(pixmap.width(), pixmap.height())
+        dataBtn = QPushButton(text="DATA", parent=self)
+        dataBtn.setFixedSize(btnWsize, btnHsize)        
+        dataBtn.setStyleSheet("color : green; border: none; font-weight: bold") 
+        dataBtn.setFont(QFont('Monofonto', btnFontSize))
 
-        # Set the central widget of the Window.
-        # self.setCentralWidget(button)
+        mapBtn = QPushButton(text="MAP", parent=self)
+        mapBtn.setFixedSize(btnWsize, btnHsize)        
+        mapBtn.setStyleSheet("color : green; border: none; font-weight: bold") 
+        mapBtn.setFont(QFont('Monofonto', btnFontSize))
+        
+        radioBtn = QPushButton(text="RADIO", parent=self)
+        radioBtn.setFixedSize(btnWsize, btnHsize)        
+        radioBtn.setStyleSheet("color : green; border: none; font-weight: bold") 
+        radioBtn.setFont(QFont('Monofonto', btnFontSize))
+
+        self.top_bar.addWidget(statBtn)
+        self.top_bar.addWidget(invBtn)
+        self.top_bar.addWidget(dataBtn)
+        self.top_bar.addWidget(mapBtn)
+        self.top_bar.addWidget(radioBtn)
 
 
 app = QApplication(sys.argv)
