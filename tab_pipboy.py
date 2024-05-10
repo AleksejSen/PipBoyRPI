6cad28405f3e852a5d11f27b537a4f47d8a4cb76
Alex <sinbar82@icloud.com>
2024-05-10 09:44:00 +0200

    Add dummy pictures, scale tabs to screen size

--- tab_pipboy.py
+++ tab_pipboy.py
@@ -10,13 +10,14 @@ from PyQt5.QtWidgets import (
     QWidget,
     QLabel,
 )
-from PyQt5.QtGui import QMovie, QPalette, QColor
+from PyQt5.QtGui import QMovie, QPalette, QColor, QPixmap
 
 class Window(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("PiP Boy")
         self.resize(480, 320)
+        self.setFixedSize(480,320)
         self.setStyleSheet("background-color: black;")
         self.setWindowFlags(Qt.FramelessWindowHint)
 
@@ -53,17 +54,18 @@ class Window(QWidget):
         self.setLayout(layout)
         # Create the tab widget with two tabs
         tabs = QTabWidget()
+        tabs.resize(420, 280)
         tabs.setStyleSheet(tabBarBarStyle)
 
-        tabs.addTab(self.statUI(), "STAT")
-        tabs.addTab(self.networkTabUI(), "INV")
-        tabs.addTab(self.networkTabUI(), "DATA")
-        tabs.addTab(self.networkTabUI(), "MAP")
-        tabs.addTab(self.networkTabUI(), "RADIO")
+        tabs.addTab(self.statTabUI(), "STAT")
+        tabs.addTab(self.invTabUI(), "INV")
+        tabs.addTab(self.dataTabUI(), "DATA")
+        tabs.addTab(self.mapTabUI(), "MAP")
+        tabs.addTab(self.radioTabUI(), "RADIO")
         layout.addWidget(tabs)
 
 
-    def statUI(self):
+    def statTabUI(self):
         """Create the General page UI."""
         tab = QWidget()
         mainWindowStyles = """
@@ -78,16 +80,62 @@ class Window(QWidget):
         layout = QVBoxLayout()
         layout.addWidget(valutBoyLable)
         tab.setLayout(layout)
+        tab.resize(420, 280)
         return tab
 
-    def networkTabUI(self):
-        """Create the Network page UI."""
-        networkTab = QWidget()
+    def invTabUI(self):
+        """Create the inv page UI."""
+        invTab = QWidget()
+        invePic = QPixmap('images/inventory.png')
+        lable = QLabel()
+        lable.setPixmap(invePic)
+        lable.setScaledContents(True)
         layout = QVBoxLayout()
-        layout.addWidget(QCheckBox("Network Option 1"))
-        layout.addWidget(QCheckBox("Network Option 2"))
-        networkTab.setLayout(layout)
-        return networkTab
+        layout.addWidget(lable)
+        invTab.setLayout(layout)
+        invTab.resize(420, 280)
+        return invTab
+
+    
+    def dataTabUI(self):
+        """Create the data page UI."""
+        dataTab = QWidget()
+        dataPic = QPixmap('images/data.png')
+        lable = QLabel()
+        lable.setPixmap(dataPic)
+        lable.setScaledContents(True)
+        layout = QVBoxLayout()
+        layout.addWidget(lable)
+        dataTab.setLayout(layout)
+        dataTab.resize(420, 280)
+        return dataTab
+    
+    def mapTabUI(self):
+        """Create the map page UI."""
+        mapTab = QWidget()
+        mapPic = QPixmap('images/map.png')
+        lable = QLabel()
+        lable.setPixmap(mapPic)
+        lable.setScaledContents(True)
+        layout = QVBoxLayout()
+        layout.addWidget(lable)
+        mapTab.setLayout(layout)
+        mapTab.resize(420, 280)
+        return mapTab
+    
+    def radioTabUI(self):
+        """Create the radio page UI."""
+        radioTab = QWidget()
+        radioPic = QPixmap('images/radio.png')
+        lable = QLabel()
+        lable.setPixmap(radioPic)
+        lable.setScaledContents(True)
+        layout = QVBoxLayout()
+        layout.addWidget(lable)
+        radioTab.setLayout(layout)
+        radioTab.resize(420, 280)
+        return radioTab
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
